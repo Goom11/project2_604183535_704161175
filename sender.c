d152b6d7704927e7bf88168e871ac5d26ff0c810
Siddhant Khemka <skhemka@ucla.edu>
2016-02-23 16:07:57 -0800

    loop

--- sender.c
+++ sender.c
@@ -1,5 +1,7 @@
 
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -22,6 +24,7 @@ int main(int argc, char *argv[])
     hints.ai_flags = AI_PASSIVE;
 
     struct addrinfo *servinfo;
+    int rv;
 
     if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
@@ -29,7 +32,30 @@ int main(int argc, char *argv[])
     }
 
     struct addrinfo *p;
-    // for(p = servinfo; p != NUL; p = p->ai.next) {
-    // }
+
+    int sockfd;
+    for(p = servinfo; p != NUL; p = p->ai.next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("sender: socket");
+            continue;
+        }
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("sender: bind");
+            continue;
+        }
+        break;
+    }
+
+    if (p == NULL){
+        fprintf(stderr, "Error: sender failed to bind socket");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo);
+
+    printf("Waiting for requested filename...\n");
+
 }
+
 
